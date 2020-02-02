e7a8a434990136bb13419f2a4e54937882f4e564
Rinesh Ramadhin <rineshramadhin@users.noreply.github.com>
2020-02-02 18:49:50 +0100

    switched to dark mode, modified where needed

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import vuetify from "./plugins/vuetify";
 import "@babel/polyfill";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
+import "./theme/overwrite.scss";
 import "./theme/general.scss";
 
 Vue.config.productionTip = false;
