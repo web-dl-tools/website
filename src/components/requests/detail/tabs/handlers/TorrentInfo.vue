1bb614d447d53c04e6ba93ce5eb7fc442be00a68
Rinesh Ramadhin <rineshramadhin@users.noreply.github.com>
2021-05-08 21:33:52 +0000

    Update src/components/requests/detail/tabs/handlers/TorrentInfo.vue

--- src/components/requests/detail/tabs/handlers/TorrentInfo.vue
+++ src/components/requests/detail/tabs/handlers/TorrentInfo.vue
@@ -3,20 +3,14 @@
     <v-col class="mb-n1" cols="12">
       <p class="mb-0 body-1">{{ item.data.name }}</p>
       <p class="font-weight-light mb-0">Hash: {{ item.data.hash }}</p>
-      <p class="font-weight-light">
-        Size: {{ formatBytes(item.data.total_size, 2) }}
-      </p>
       <p class="font-weight-light mb-0">Tracker: {{ item.data.tracker }}</p>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import formatters from "../../../../../mixins/formatters";
-
 export default {
   name: "components.requests.detail.tabs.handlers.torrent-info",
-  mixin: [formatters],
   props: {
     item: Object,
   },
