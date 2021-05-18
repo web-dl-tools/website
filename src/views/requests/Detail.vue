15828be472d6ad9f6b91e6a4f8629d419450038e
Rinesh Ramadhin <rinesh.ramadhin+gitlab@outlook.com>
2021-05-18 15:33:29 +0000

    Update src/views/requests/Detail.vue

--- src/views/requests/Detail.vue
+++ src/views/requests/Detail.vue
@@ -110,7 +110,7 @@
         <v-card-subtitle class="subtitle-2">
           Are you sure you want to delete the following request?
         </v-card-subtitle>
-        <v-card-text class="pb-3 font-italic info--text">
+        <v-card-text class="pb-3 info--text">
           {{ request.status === "completed" ? request.title : request.url }}
         </v-card-text>
         <v-card-actions>
